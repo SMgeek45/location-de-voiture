86a2da8af5e78a971410bdf6d4cf764a01b768bd
stephane <stephane.m450@gmail.com>
2021-10-04 10:05:26 +0200

    main

---
+++ Account/delete_account.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+require_once("");
+if() {};
+if() {};
+else() {};
+try {},
+catch {};
+die {};
+?>
---
+++ Index/voitures.sql
@@ -0,0 +1,217 @@
+INSERT INTO `cars` (`matricule`, `marque`, `modele`, `couleur`, `puissance`, `price`,)
+/*Pour le prix : prix d'origine */
+VALUES
+  ('YV-593-OB', 'VolksWagen', 'Polo', 'gris cendré', '80ch', '',),
+  ('FU-854-DC', 'VolksWagen', 'Golf', 'bleu récif', '95ch', '',),
+  ('OS-354-QD', 'VolksWagen', 'Taigo', 'orange energy', '85ch', '',),
+  ('JQ-521-ZN', 'VolksWagen', 'T-Cross', 'turquoise maui', '95ch', '',),
+  ('UL-937-VE', 'VolksWagen', 'T-Roc', 'jaune curcuma', '110ch', '',),
+  ('HE-045-HC', 'VolksWagen', 'Tiguan', 'nightshade blue', '130ch', '',),
+  ('OF-853-TD', 'VolksWagen', 'Passat', 'rouge tornado', '122ch', '',),
+  ('AD-948-BX', 'VolksWagen', 'Touran', 'gris dauphin', '150ch', '',),
+  ('OG-735-NT', 'VolksWagen', 'Arteon', 'gris pyrite', '190ch', '',),
+  ('IC-174-FA', 'VolksWagen', 'Caddy', 'copper bronze métallisé', '75ch', '',),
+  ('HB-969-YT', 'VolksWagen', 'Caravelle', 'bay leaf green mettalisé', '110ch', '',),
+  ('JX-748-DQ', 'VolksWagen', 'California', 'mojave beige métallisé', '110ch', '',),
+  ('UA-530-DS', 'VolksWagen', 'Multivan', 'costa azul metallisé', '102ch', '',),
+  ('NR-831-GY', 'Citroen', 'Ami', 'khaki', '220 volts', '',),
+  ('HZ-677-BW', 'Citroen', 'C1', 'rouge scarlet', '53kW', '',),
+  ('AA-221-TC', 'Citroen', 'C3 Aircross', 'blanc banquise', '81kW', '',),
+  ('IB-789-YW', 'Citroen', 'C4', 'brun caramel nacré', '75kW', '',),
+  ('KE-824-QH', 'Citroen', 'C5 X', 'gris amazonite', '96kW', '',),
+  ('DP-942-HF', 'Citroen', 'E-Berlingo', 'gris platinium métallisé', '81kw', '',),
+  ('HV-240-YX', 'Citroen', 'Spacetourer', 'gris acier', '88kW', '',),
+  ('AS-878-PJ', 'Hyundai', 'Kona', 'teal', '100kW /136ch', '',),
+  ('GR-244-BF', 'Hyundai', 'Ioniq', 'fiery red', '100kW /136ch', '',),
+  ('MN-030-CE', 'Hyundai', 'Nexo', 'phantom black', '100kW /136ch', '',),
+  ('QW-613-JI', 'Hyundai', 'Santa Fe', 'magma red', '169kW 230ch', '',),
+  ('LA-275-YF', 'Hyundai', 'Tucson', 'silky bronze', '195kW / 265ch', '',),
+  ('OR-849-UC', 'Hyundai', 'i10', 'brass', '49,3kW /67ch', '',),
+  ('OA-121-GC', 'Hyundai', 'i20', 'intense blue', '61,8kW / 84ch', '',),
+  ('HZ-823-CW', 'Hyundai', 'i30', 'dark knight', '88,3kW /120ch', '',),
+  ('UC-014-NX', 'Hyundai', 'i30 Fastback', 'shimmering silver', '117kW /159ch', '',),
+  ('AO-736-VF', 'Hyundai', 'Bayon', 'mangrove green', '73,6 kW /84ch', '',),
+  ('SI-614-EA', 'Toyota', 'Aygo', 'gris atlas', '72ch', '',),
+  ('ZM-917-TC', 'Toyota', 'Yaris', 'blanc lunaire nacré', '116 ch', '',),
+  ('YC-210-ID', 'Toyota', 'Yaris Cross', 'bi-ton or pur / toit noir', '116ch', '',),
+  ('LZ-253-BE', 'Toyota', 'Corolla', 'brun ébène', '122ch', '',),
+  ('PC-703-RV', 'Toyota', 'C-HR', 'bleu nebula', '122ch', '',),
+  ('FV-189-KZ', 'Toyota', 'Rav4', 'rouge allure nacré', '218ch', '',),
+  ('KZ-372-HA', 'Toyota', 'Highlander', 'bleu de prusse métallisé', '248ch', '',),
+  ('XR-011-XU', 'Toyota', 'Prius', 'gris Pluton métallisé', '122', '',),
+  ('NO-238-TW', 'Toyota', 'Camry', 'bleu kyanite métallisé', '218', '',),
+  ('BF-193-GD', 'Toyota', 'Mirai', 'blanc quartz perlé', '182', '',),
+  ('OP-646-UY', 'Toyota', 'GR Supra', 'lightning yellow', '258ch', '',),
+  ('BQ-829-IA', 'Toyota', 'Proace', 'noir perla nera', '1.5 D-4D 100', '',),
+  ('GR-853-ID', 'Seat', 'Mii electric', 'rouge tornado / toit noir', '61Kw', '',),
+  ('BT-992-JA', 'Seat', 'Ibiza', 'bleu', '95ch', '',),
+  ('IW-795-KI', 'Seat', 'Leon', 'gris', '150ch', '',),
+  ('MM-253-BS', 'Seat', 'Alteca', 'noir', '110', '',),
+  ('IX-934-OH', 'Seat', 'Arona', 'blanc candy', '95ch', '',),
+  ('BP-863-RJ', 'Seat', 'Tarraco', 'gris urano', '150ch', '',),
+  ('SO-042-UU', 'Mercedes', 'classe A', 'or rose métallisé', '80kW /109ch', '',),
+  ('VB-674-DY', 'Mercedes', 'classe B', 'rouge patagonie métallisé', '80kW /109ch', '',),
+  ('LH-962-TV', 'Mercedes', 'classe C', 'rouge jacinthe métallisé', '120k+80kW / 163+20ch', '',),
+  ('VD-130-XA', 'Mercedes', 'classe E', 'noir obsidienne métallisé', '143kW /194ch', '',),
+  ('HP-917-KY', 'Mercedes', 'classe V', 'vert granite', '120kW /163ch', '',),
+  ('VZ-002-IN', 'Mercedes', 'classe S limousine', 'blanc cahchemire magno designo', '210kW / 286ch', '',),
+  ('RC-749-JE', 'Mercedes', 'CLA shooting brake', 'bleu denim métallisé', '100kW / 136ch', '',),
+  ('SU-374-IB', 'Mercedes', 'EQA', 'noir cosmos','140 kW / 190ch', '',),
+  ('LZ-448-FD', 'Mercedes', 'EQB', 'noir cosmos','140 kW / 190ch', '',),
+  ('EU-348-AO', 'Mercedes', 'EQC', 'noir cosmos','140 kW / 190ch', '',),
+  ('ML-135-GS', 'Mercedes', 'EQE', 'noir', '215 kW / 292ch', '',),
+  ('UR-974-NQ', 'Mercedes', 'EQS', 'bleu sodalithe', '245kW / 333ch', '',),
+  ('RX-284-EZ', 'Mercedes', 'GLA', 'argent iridium métallisé', '120 kW / 163ch', '',),
+  ('KE-853-YF', 'Mercedes', 'GLB', 'blanc digital métallisé', '120 kW / 163ch', '',),
+  ('VC-479-OR', 'Mercedes', 'GLC coupé', 'gris sélénite magno designo', '120 kW / 163ch', '',),
+  ('RD-371-FB', 'Mercedes', 'GLS', 'bleu cavansite métallisé', '243 kW /330ch', '',),
+  ('OX-364-DH', 'Mercedes', 'AMG GT coupé', 'bleu spectral métallisé', '270 kW+16kW / 367+22ch ', '',),
+  ('XR-111-XK', 'Skoda', 'Enyaq iV', 'gris arctic', '52kW', '',),
+  ('NO-338-TX', 'Skoda', 'Kodiaq', 'bleu énergie', '150ch', '',),
+  ('BF-293-GF', 'Skoda', 'Fabia', 'blanc cristal', '65ch', '',),
+  ('OP-546-UU', 'Skoda', 'Scala', 'rouge velvet', '95ch', '',),
+  ('BQ-729-IE', 'Skoda', 'Octavia', 'bleu lave', '110ch', '',),
+  ('GR-953-IF', 'Skoda', 'Superb Sportline', 'noir magic nacré', '150ch', '',),
+  ('BT-092-JZ', 'Skoda', 'Kamiq', 'gris perle', '110ch', '',),
+  ('IW-095-KO', 'Skoda', 'Karoq', 'gris argent', '110ch', '',),
+  ('MM-253-BD', 'Alfa Romeo', 'Giulia', 'vert visconti', '200', '',),
+  ('IX-034-OJ', 'Alfa Romeo', 'Stelvio Quadrifolio', 'ocre et junior', '510ch', '',),
+  ('BP-763-RK', 'Alfa Romeo', 'Giulietta', 'bleu adriatico', '120ch', '',),
+  ('SP-042-UO', 'Fiat', '500', 'celestial blue', '87kW / 118ch', '',),
+  ('VN-674-DI', 'Fiat', '500 X', 'blue venezia', '120ch', '',),
+  ('LJ-962-TN', 'Fiat', '500 L', 'gris moda / toit noir brillant', '95ch', '',),
+  ('VF-130-XE', 'Fiat', 'Tipo Cross', 'bronze magnetico', '100ch', '',),
+  ('HQ-917-KI', 'Fiat', 'Tipo Berline', 'gris colosseo', '100ch', '',),
+  ('VE-002-IE', 'Fiat', 'Panda life', 'blanc gelato', '70ch', '',),
+  ('RC-749-JR', 'Opel', 'Zafira-E Life', 'blanc métallisé', '136ch', '',),
+  ('SU-364-IN', 'Opel', 'Vivaro', 'gris métallisé', '120ch', '',),
+  ('LZ-438-FF', 'Opel', 'Mokka', 'vert métallisé', '100ch', '',),
+  ('EU-338-AP', 'Opel', 'Crossland', 'orange métallisé', '83ch', '',),
+  ('ML-125-GD', 'Opel', 'Movano', 'blanc métallisé', '135ch', '',),
+  ('UR-964-NS', 'Opel', 'Insignia Grand sport', 'gris métallisé', '122ch', '',),
+  ('RX-274-EE', 'Opel', 'Grandland X', 'bleu métalisé premium', '225ch', '',),
+  ('OX-364-DK', 'Opel', 'Astra', 'rouge', '110ch', '',),
+  ('RD-371-FA', 'Opel', 'Combo Cargo', 'blanc', '110ch', '',),
+  ('VC-479-OY', 'Opel', 'Combo Life', 'blanc', '110ch', '',),
+  ('KE-853-YH', 'Opel', 'Corsa-E', 'orange métallisé', '100Kw /100ch', '',),
+  ('YV-593-OB', 'Audi', 'e-tron', 'gris typhon', '230kW', '',),
+  ('FU-854-DV', 'Audi', 'A1', 'jaune python', '70 kW / 95 ch', '',),
+  ('OS-354-QF', 'Audi', 'A3', 'bleu atoll métallisé', '81kW /110ch', '',),
+  ('JQ-521-ZA', 'Audi', 'A4', 'bleu navarre', '110kw / 150ch', '',),
+  ('UL-937-VR', 'Audi', 'A5', 'vert district', '110kw / 150ch', '',),
+  ('HE-045-HV', 'Audi', 'A6', 'rouge tango', '120kw / 163ch', '',),
+  ('OF-853-TF', 'Audi', 'A7', 'gris chronos', '150kW / 204ch', '',),
+  ('AD-948-BC', 'Audi', 'A8', 'noir mythe métallisé', '81kW / 110ch', '',),
+  ('OG-735-NY', 'Audi', 'Q2', '', 'kW / ch', '',),
+  ('IC-174-FZ', 'Audi', 'Q3', '', 'kW / ch', '',),
+  ('HB-969-YU', 'Audi', 'Q4 e-tron', '', 'kW / ch', '',),
+  ('JX-748-DS', 'Audi', 'Q5', '', 'kW / ch', '',),
+  ('UA-530-DD', 'Audi', 'Q7', 'beige vigogne', '280kW / 381ch', '',),
+  ('NR-831-GU', 'Audi', 'Q8', 'orange dragon', '280kW / 381ch', '',),
+  ('HZ-677-BW', 'Audi', 'TT', 'blacn ibis', '145kW / 197ch', '',),
+  ('GZ-682-BW', 'Audi', 'R8', 'gris', '570ch', '',),
+  ('AA-220-TC', 'Renault', 'Talisman', 'rouge millésime', '160ch', '',),
+  ('IB-788-YW', 'Renault', 'Espace', 'noir étoilé', '160ch', '',),
+  ('KE-823-QH', 'Renault', 'Trafic Combi', 'rouge carmin', '150ch', '',),
+  ('DP-941-HF', 'Renault', 'Kangoo', 'brun terracotta', '100ch', '',),
+  ('HV-249-YX', 'Renault', 'Kadjar', 'rouge flamme', '140ch', '',),
+  ('AS-877-PJ', 'Renault', 'Clio', 'bleu celadon', '140ch', '',),
+  ('GR-243-BF', 'Renault', 'Twingo E-tech', 'bleu dragée', '60kW /81ch', '',),
+  ('MN-039-CE', 'Renault', 'Captur', 'gris cassoipée', '145ch', '',),
+  ('QW-612-JI', 'Renault', 'Megane E-tech hybride', 'bleu nocturne', '130ch', '',),
+  ('LA-274-YF', 'Renault', 'Megane Estate', 'bleu iron', '160ch', '',),
+  ('OR-848-UC', 'Renault', 'Arkana', 'bleu zanzibar', '145ch', '',),
+  ('OA-120-GC', 'Renault', 'Scenic', 'bleu cosmos', '115ch', '',),
+  ('HZ-822-CW', 'Renault', 'Koleos', 'noir métal', '160ch', '',),
+  ('UC-013-AX', 'Peugeot', 'Traveller', 'brun rich oak', '106kW', '',),
+  ('AO-735-CF', 'Peugeot', 'Expert Combi', 'gris artense', '88kW', '',),
+  ('SI-613-ZA', 'Peugeot', 'E-Rifter', 'rouge ral 3000', '81kW', '',),
+  ('ZM-916-RC', 'Peugeot', '5008', 'metallic copper', '96kW', '',),
+  ('YC-219-UD', 'Peugeot', '508', 'dark blue', '133kW', '',),
+  ('LZ-252-VE', 'Peugeot', '3008', 'bleu célèbes', '96kW', '',),
+  ('PC-702-XV', 'Peugeot', '308', ' Vert Olivine', '225ch', '',),
+  ('FV-188-JZ', 'Peugeot', '2008', 'orange fusion', '100kW / 136ch', '',),
+  ('KZ-371-GA', 'Peugeot', '208', 'jaune faro', '55kW', '',),
+  ('AA-221-YC', 'IVM', 'G5', '', '', '',),
+  ('IB-789-UW', 'Kiira', 'Motors', '', '', '',),
+  ('KE-824-SH', 'Nissan', 'NV400', '', '', '',),
+  ('DP-942-JF', 'Nissan', 'NV300', '', '', '',),
+  ('HV-240-BX', 'Nissan', 'NV250', '', '', '',),
+  ('AS-878-FJ', 'Nissan', 'GT-R', '', '', '',),
+  ('GR-244-EF', 'Nissan', 'E-NV200 Evalia', '', '', '',),
+  ('MN-030-VE', 'Nissan', 'Ariya', '', '', '',),
+  ('QW-613-MI', 'Nissan', 'Micra', 'ivoire', '92kW', '',),
+  ('LA-275-OF', 'Nissan', 'Juke', 'bronze intense', '114kW', '',),
+  ('OR-849-AC', 'Nissan', 'Leaf', 'bleu topaze', '', '',),
+  ('OA-121-WC', 'Nissan', 'Qashqai', 'rouge passion', '140ch', '',),
+  ('HZ-833-FW', 'Mazda', '2', 'eternal blue mica', '66kW / 90ch', '',),
+  ('UC-024-NX', 'Mazda', '3', 'polymetal gray métallisé', '90kW / 122ch', '',),
+  ('AO-746-VF', 'Mazda', '6', 'snowflake white pearl mica', '143kW / 194ch', ''),
+  ('SI-634-EA', 'Mazda', 'CX-30', 'titanium flash mica', '90kW / 122ch', '',),
+  ('ZM-927-TC', 'Mazda', 'CV-5', 'soul red crytal', '121kW / 165ch', '',),
+  ('YC-210-ID', 'Mazda', 'MV-30', 'ceramic white métallisé', '107kW / 145ch', '',),
+  ('MZ-253-BE', 'Volvo', 'C40', 'gris tonnerre métallisé', '300kW / 408ch', '',),
+  ('QC-703-RV', 'Volvo', 'XC40', 'vert sauge métallisé', '170kW / 231ch', '',),
+  ('GV-189-KZ', 'Volvo', 'XC60', 'gris épicéa métallisé', '145kW / 197ch', '',),
+  ('LZ-372-HA', 'Volvo', 'XC90', 'gris pebble métallisé', '173kW / 235ch', '',),
+  ('CR-011-XU', 'Volvo', 'V60', 'rouge fusion métallisé', '120kW / 163ch', '',),
+  ('AO-238-TW', 'Volvo', 'V90', 'noir onyx métallisé', '145kW / 197ch', '',),
+  ('NF-193-GD', 'Volvo', 'S60', 'blanc glace 614', '120kW / 163ch', '',),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
